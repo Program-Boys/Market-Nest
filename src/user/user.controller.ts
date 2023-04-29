e9682cbac67995ecc7621382e09086e90811bb56
Enrique <enrique.barbosasilva@gmai.com>
2023-04-29 15:15:00 -0300

    List user by id controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,32 @@
-import { Controller, Get, Post, Put, Delete, Res, Body } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, Post, Put, Delete, Res, Body, Req } from '@nestjs/common';
+import { Response, Request } from 'express';
 import { UserServices } from './user.service';
 import { UserBodyDTO } from './dto/user.dto';
 
 @Controller('/user')
 export class UserController {
   constructor(private readonly userService: UserServices) {}
-
+  
+  @Post()
+  async createUser(@Res() res: Response, @Body() body: UserBodyDTO) {
+    const createUser = await this.userService.createUser(body);
+    
+    return res.status(201).json(createUser);
+  }
+  
   @Get()
   async getUser(@Res() res: Response) {
-    const users = await this.userService.listUser();
+    const users = await this.userService.listUsers();
 
     return res.status(200).json(users);
   }
 
-  @Post()
-  async createUser(@Res() res: Response, @Body() body: UserBodyDTO) {
-    const createUser = await this.userService.createUser(body);
+  @Get('/:id')
+  async getUserById(@Res() res: Response, @Req() req: Request) {
+    const { id } = req.params
 
-    return res.status(201).json(createUser);
+    const user = await this.userService.listUser(id)
+
+    return res.status(200).json(user)
   }
 }
