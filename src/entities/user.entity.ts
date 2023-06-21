063f8a0021c04692125aa1c536e63d7e33369d31
Guilherme <guilhermeshadow91@gmail.com>
2023-06-21 17:01:55 -0300

    entities to be used in tests

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,25 @@
+const allUsers: User[] = [];
+
+class User {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  cpf: string;
+
+  private constructor({ name, email, password, cpf }: User) {
+    return Object.assign(this, { name, email, password, cpf });
+  }
+
+  static create({ name, email, password, cpf }: User) {
+    const newUser = new User({ name, email, password, cpf });
+    allUsers.push(newUser);
+    return newUser;
+  }
+
+  static list(): User[] {
+    return allUsers;
+  }
+}
+
+export { User };
