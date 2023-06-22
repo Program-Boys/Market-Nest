0beb6018bbcb10aaba0dc9692503efdefee013fc
Guilherme <guilhermeshadow91@gmail.com>
2023-06-22 13:51:41 -0300

    adding new const returns

--- src/utils/return-messages/product-returns.utils.ts
+++ src/utils/return-messages/product-returns.utils.ts
@@ -1,3 +1,3 @@
 export const MP_PRODUCT_DELETED = 'Product deleted';
 export const MP_PRODUCT_REMOVED_FROM_CART = 'Product removed from cart';
-export const MP_PRODUCT_MID_NOT_FOUND = 'Product not found';
+export const MP_PRODUCT_NOT_FOUND = 'Product not found';
