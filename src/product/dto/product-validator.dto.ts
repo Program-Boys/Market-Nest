87105ef84344c1c7c4c441972f39238f324fcde5
Guilherme <guilhermeshadow91@gmail.com>
2023-06-26 15:37:49 -0300

    Product validator to validate the size of the img

---
+++ src/product/dto/product-validator.dto.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import {
+  ValidatorConstraint,
+  ValidationArguments,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'urlSiz', async: true })
+export class UrlSizeValidator implements ValidatorConstraintInterface {
+  async validate(url: string, args: ValidationArguments): Promise<boolean> {
+    try {
+      const response = await axios.head(url);
+      const imgSize = response.headers['content-length'];
+      const sizeInBytes = parseInt(imgSize, 10);
+      const maxSize = 10 * 1024 * 1024;
+
+      return sizeInBytes <= maxSize;
+    } catch (err) {
+      return false;
+    }
+  }
+  defaultMessage(args: ValidationArguments): string {
+    return 'URL size exceeds 10MB';
+  }
+}
